ece3818fdc0092b2ea23674af199d6fb733fdb78
Nishkarshsh <42824542+Nishkarshsh@users.noreply.github.com>
2020-06-21 20:16:02 +0530

    Update code.h

--- C_Mavlink_API/code.h
+++ C_Mavlink_API/code.h
@@ -1,1 +1,201 @@
 
+
+#ifndef AUTOPILOT_INTERFACE_H_
+#define AUTOPILOT_INTERFACE_H_
+
+// ------------------------------------------------------------------------------
+//   Includes
+// ------------------------------------------------------------------------------
+
+#include "generic_port.h"
+
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h> // This uses POSIX Threads
+#include <unistd.h>  // UNIX standard function definitions
+
+#include <common/mavlink.h>
+
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION     0b0000110111111000
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_VELOCITY     0b0000110111000111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_ACCELERATION 0b0000110000111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_FORCE        0b0000111000111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_ANGLE    0b0000100111111111
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_YAW_RATE     0b0000010111111111
+
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_TAKEOFF      0x1000
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_LAND         0x2000
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_LOITER       0x3000
+#define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_IDLE         0x4000
+
+// ------------------------------------------------------------------------------
+//   Prototypes
+// ------------------------------------------------------------------------------
+
+
+// helper functions
+uint64_t get_time_usec();
+void set_position(float x, float y, float z, mavlink_set_position_target_local_ned_t &sp);
+void set_velocity(float vx, float vy, float vz, mavlink_set_position_target_local_ned_t &sp);
+void set_acceleration(float ax, float ay, float az, mavlink_set_position_target_local_ned_t &sp);
+void set_yaw(float yaw, mavlink_set_position_target_local_ned_t &sp);
+void set_yaw_rate(float yaw_rate, mavlink_set_position_target_local_ned_t &sp);
+
+void* start_autopilot_interface_read_thread(void *args);
+void* start_autopilot_interface_write_thread(void *args);
+
+
+// ------------------------------------------------------------------------------
+//   Data Structures
+// ------------------------------------------------------------------------------
+
+struct Time_Stamps
+{
+	Time_Stamps()
+	{
+		reset_timestamps();
+	}
+
+	uint64_t heartbeat;
+	uint64_t sys_status;
+	uint64_t battery_status;
+	uint64_t radio_status;
+	uint64_t local_position_ned;
+	uint64_t global_position_int;
+	uint64_t position_target_local_ned;
+	uint64_t position_target_global_int;
+	uint64_t highres_imu;
+	uint64_t attitude;
+
+	void
+	reset_timestamps()
+	{
+		heartbeat = 0;
+		sys_status = 0;
+		battery_status = 0;
+		radio_status = 0;
+		local_position_ned = 0;
+		global_position_int = 0;
+		position_target_local_ned = 0;
+		position_target_global_int = 0;
+		highres_imu = 0;
+		attitude = 0;
+	}
+
+};
+
+
+// Struct containing information on the MAV we are currently connected to
+
+struct Mavlink_Messages {
+
+	int sysid;
+	int compid;
+
+	// Heartbeat
+	mavlink_heartbeat_t heartbeat;
+
+	// System Status
+	mavlink_sys_status_t sys_status;
+
+	// Battery Status
+	mavlink_battery_status_t battery_status;
+
+	// Radio Status
+	mavlink_radio_status_t radio_status;
+
+	// Local Position
+	mavlink_local_position_ned_t local_position_ned;
+
+	// Global Position
+	mavlink_global_position_int_t global_position_int;
+
+	// Local Position Target
+	mavlink_position_target_local_ned_t position_target_local_ned;
+
+	// Global Position Target
+	mavlink_position_target_global_int_t position_target_global_int;
+
+	// HiRes IMU
+	mavlink_highres_imu_t highres_imu;
+
+	// Attitude
+	mavlink_attitude_t attitude;
+
+	// System Parameters?
+
+
+	// Time Stamps
+	Time_Stamps time_stamps;
+
+	void
+	reset_timestamps()
+	{
+		time_stamps.reset_timestamps();
+	}
+
+};
+
+
+class Autopilot_Interface
+{
+
+public:
+
+	Autopilot_Interface();
+	Autopilot_Interface(Generic_Port *port_);
+	~Autopilot_Interface();
+
+	char reading_status;
+	char writing_status;
+	char control_status;
+    uint64_t write_count;
+
+    int system_id;
+	int autopilot_id;
+	int companion_id;
+
+	Mavlink_Messages current_messages;
+	mavlink_set_position_target_local_ned_t initial_position;
+
+	void update_setpoint(mavlink_set_position_target_local_ned_t setpoint);
+	void read_messages();
+	int  write_message(mavlink_message_t message);
+
+	int	 arm_disarm( bool flag );
+	void enable_offboard_control();
+	void disable_offboard_control();
+
+	void start();
+	void stop();
+
+	void start_read_thread();
+	void start_write_thread(void);
+
+	void handle_quit( int sig );
+
+
+private:
+
+	Generic_Port *port;
+
+	bool time_to_exit;
+
+	pthread_t read_tid;
+	pthread_t write_tid;
+
+	mavlink_set_position_target_local_ned_t current_setpoint;
+
+	void read_thread();
+	void write_thread(void);
+
+	int toggle_offboard_control( bool flag );
+	void write_setpoint();
+
+};
+
+
+
+#endif // AUTOPILOT_INTERFACE_H_
+
